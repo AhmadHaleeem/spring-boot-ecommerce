ba5d6495e7045f902f2c11c3d56236f577ac02a3
AhmadHaleeem <coldman2099@gmail.com>
2020-07-17 18:08:57 +0200

    expose_all_categories_in_home_page

--- src/main/java/com/haleem/ecommerce/helpers/Common.java
+++ src/main/java/com/haleem/ecommerce/helpers/Common.java
@@ -7,7 +7,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.haleem.ecommerce.models.Category;
 import com.haleem.ecommerce.models.Page;
+import com.haleem.ecommerce.repositories.CategoryRepository;
 import com.haleem.ecommerce.repositories.PageRepository;
 
 @ControllerAdvice
@@ -15,13 +17,18 @@ public class Common {
 
 	@Autowired
 	private PageRepository pageRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@ModelAttribute
 	public void sharedData(Model model) {
 		List<Page> pages = pageRepository.findAllByOrderBySortingAsc();
-
+		List<Category> categories = categoryRepository.findAll();
+		
 		model.addAttribute("cpages", pages);
-
+		model.addAttribute("ccategories", categories);
 	}
+
 
 }
