128b0598ed2396ed6e1483cfb618c50ebe306954
AhmadHaleeem <coldman2099@gmail.com>
2020-07-17 17:23:39 +0200

    adding_pagination_to_the_main_page

--- src/main/java/com/haleem/ecommerce/controller/AdminProductsController.java
+++ src/main/java/com/haleem/ecommerce/controller/AdminProductsController.java
@@ -10,6 +10,9 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -36,8 +40,13 @@ public class AdminProductsController {
 	private CategoryRepository categoryRepository;
 
 	@GetMapping
-	public String index(Model model) {
-		List<Product> products = productRepository.findAll();
+	public String index(Model model, @RequestParam(value = "page", required = false) Integer p) {
+
+		int perPage = 6;
+		int page = (p != null) ? p : 0;
+
+		Pageable pageable = PageRequest.of(page, perPage);
+		Page<Product> products = productRepository.findAll(pageable);
 		model.addAttribute("products", products);
 
 		List<Category> categories = categoryRepository.findAll();
@@ -46,9 +55,15 @@ public class AdminProductsController {
 		for (Category category : categories) {
 			cats.put(category.getId(), category.getName());
 		}
-
 		model.addAttribute("cats", cats);
 
+		Long count = productRepository.count();
+		double pageCount = Math.ceil((double) count / (double) perPage);
+		model.addAttribute("pageCount", (int) pageCount);
+		model.addAttribute("perPage", perPage);
+		model.addAttribute("count", count);
+		model.addAttribute("page", page);
+		
 		return "/admin/products/index";
 	}
 
@@ -158,11 +173,11 @@ public class AdminProductsController {
 			redirectAttributes.addFlashAttribute("product", product);
 		} else {
 			product.setSlug(slug);
-			
+
 			if (!file.isEmpty()) {
 				Path path2 = Paths.get("src/main/resources/static/media/" + currentProduct.getImage());
 				Files.delete(path2);
-				
+
 				product.setImage(fileName);
 				Files.write(path, bytes);
 			} else {
@@ -173,6 +188,22 @@ public class AdminProductsController {
 		}
 
 		return "redirect:/admin/products/edit/" + product.getId();
+	}
+
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable int id, RedirectAttributes redirectAttributes) throws IOException {
+		Product product = productRepository.getOne(id);
+		Product currentProduct = productRepository.getOne(product.getId());
+
+		productRepository.deleteById(id);
+
+		Path path2 = Paths.get("src/main/resources/static/media/" + currentProduct.getImage());
+		Files.delete(path2);
+
+		redirectAttributes.addFlashAttribute("message", "Product deleted successfully..");
+		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+		return "redirect:/admin/products";
 	}
 
 }
--- src/main/java/com/haleem/ecommerce/repositories/ProductRepository.java
+++ src/main/java/com/haleem/ecommerce/repositories/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.haleem.ecommerce.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.haleem.ecommerce.models.Product;
@@ -9,5 +11,7 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 	Product findBySlug(String slug);
 
 	Product findBySlugAndIdNot(String slug, int id);
+	
+	Page<Product> findAll(Pageable pageable);
 
 }
